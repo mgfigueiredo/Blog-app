4e1db4d0ca255c0d5f6ccea24eb45197dc475a9b
Malcolm Figueiredo <mgfigueiredo15@gmail.com>
2020-08-01 10:16:47 +0100

    Fixed tagging and authentication bugs and changed database from Sqlite3 to PostgreSQL

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,7 @@
 module ArticlesHelper
 
     def article_params
-        params.require(:article).permit(:title, :body, :tag_lis, :image)
+        params.require(:article).permit(:title, :body, :tag_list, :image)
     end
 
 end
